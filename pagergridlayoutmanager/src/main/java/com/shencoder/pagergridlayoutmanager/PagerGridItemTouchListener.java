fedf272a80e981a83498f51ebcc8bbb45b5faeb5
ShenBen <714081644@qq.com>
2021-10-20 18:07:25 +0800

    滑动冲突方法优化

--- pagergridlayoutmanager/src/main/java/com/shencoder/pagergridlayoutmanager/PagerGridItemTouchListener.java
+++ pagergridlayoutmanager/src/main/java/com/shencoder/pagergridlayoutmanager/PagerGridItemTouchListener.java
@@ -18,8 +18,9 @@ class PagerGridItemTouchListener extends RecyclerView.SimpleOnItemTouchListener 
 
     private final PagerGridLayoutManager layoutManager;
     private final RecyclerView recyclerView;
-    private float startX;
-    private float startY;
+    private int mScrollPointerId;
+    private int mInitialTouchX;
+    private int mInitialTouchY;
 
     PagerGridItemTouchListener(PagerGridLayoutManager layoutManager, RecyclerView recyclerView) {
         this.layoutManager = layoutManager;
@@ -28,43 +29,61 @@ class PagerGridItemTouchListener extends RecyclerView.SimpleOnItemTouchListener 
 
     @Override
     public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        final int actionMasked = e.getActionMasked();
+        final int actionIndex = e.getActionIndex();
         if (PagerGridLayoutManager.DEBUG) {
-            Log.i(TAG, "onInterceptTouchEvent-action: " + e.getAction());
+            Log.i(TAG, "onInterceptTouchEvent-actionMasked: " + actionMasked + ", actionIndex: " + actionIndex);
         }
-        int action = e.getAction();
-        switch (action) {
+        switch (actionMasked) {
             case MotionEvent.ACTION_DOWN:
-                startX = e.getX();
-                startY = e.getY();
-                recyclerView.getParent().requestDisallowInterceptTouchEvent(true);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                float x = e.getX();
-                float y = e.getY();
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                mScrollPointerId = e.getPointerId(actionIndex);
+                mInitialTouchX = (int) (e.getX(actionIndex) + 0.5f);
+                mInitialTouchY = (int) (e.getY(actionIndex) + 0.5f);
 
-                float disX = Math.abs(x - startX);
-                float disY = Math.abs(y - startY);
-                if (disX > disY) {
-                    if (layoutManager.canScrollHorizontally()) {
-                        recyclerView.getParent().requestDisallowInterceptTouchEvent(recyclerView.canScrollHorizontally((int) (startX - x)));
-                    } else {
-                        recyclerView.getParent().requestDisallowInterceptTouchEvent(false);
-                    }
-                } else {
-                    if (layoutManager.canScrollVertically()) {
-                        recyclerView.getParent().requestDisallowInterceptTouchEvent(recyclerView.canScrollVertically((int) (startY - y)));
-                    } else {
-                        recyclerView.getParent().requestDisallowInterceptTouchEvent(false);
-                    }
+                recyclerView.getParent().requestDisallowInterceptTouchEvent(true);
+            }
+            break;
+            case MotionEvent.ACTION_MOVE: {
+                final int index = e.findPointerIndex(mScrollPointerId);
+                if (index < 0) {
+                    return false;
                 }
-                startX = x;
-                startY = y;
-                break;
+                final int x = (int) (e.getX(index) + 0.5f);
+                final int y = (int) (e.getY(index) + 0.5f);
+
+                final int dx = x - mInitialTouchX;
+                final int dy = y - mInitialTouchY;
+
+                if (layoutManager.canScrollHorizontally()) {
+                    recyclerView.getParent().requestDisallowInterceptTouchEvent(recyclerView.canScrollHorizontally(-dx));
+                }
+                if (layoutManager.canScrollVertically()) {
+                    recyclerView.getParent().requestDisallowInterceptTouchEvent(recyclerView.canScrollVertically(-dy));
+                }
+            }
+            break;
+            case MotionEvent.ACTION_POINTER_UP: {
+                onPointerUp(e);
+            }
+            break;
             case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                recyclerView.getParent().requestDisallowInterceptTouchEvent(false);
-                break;
+            case MotionEvent.ACTION_CANCEL: {
+                //do nothing
+            }
+            break;
         }
         return false;
+    }
+
+    private void onPointerUp(MotionEvent e) {
+        final int actionIndex = e.getActionIndex();
+        if (e.getPointerId(actionIndex) == mScrollPointerId) {
+            // Pick a new pointer to pick up the slack.
+            final int newIndex = actionIndex == 0 ? 1 : 0;
+            mScrollPointerId = e.getPointerId(newIndex);
+            mInitialTouchX = (int) (e.getX(newIndex) + 0.5f);
+            mInitialTouchY = (int) (e.getY(newIndex) + 0.5f);
+        }
     }
 }
